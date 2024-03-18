828e89eb78a10a116d956b672a0d10c95ec488a1
mhayesCS <97618624+mhayesCS@users.noreply.github.com>
2024-03-18 18:32:11 -0400

    added navbar and pages

--- App.js
+++ App.js
@@ -4,12 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>WatchBox</Text>
       <StatusBar style="auto" />
     </View>
-
-    
-
 
   );
 }
@@ -17,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
   },
