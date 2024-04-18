500099316325900de70866d05b85b1ef5b5917b7
Daniel Boyce <diboyce21students.desu.edu@Daniels-MacBook-Pro-2.local>
2024-04-18 13:09:21 -0400

    updated tab bar icons

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomePage from './src/app/home';
 import ProfilePage from './src/app/Profile';
 import MediaDetailsScreen from './src/app/MediaDetails';
-
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
 
@@ -15,7 +14,15 @@ const Tab = createBottomTabNavigator();
 const HomeScreenStack = ()=> {
   return(
     <HomeStack.Navigator>
-      <HomeStack.Screen name='Home' component={HomePage}/>
+      <HomeStack.Screen name='Home' component={HomePage} options={{
+        headerStyle:{
+          backgroundColor:"#2F2F2F",
+          
+        },
+        headerTitleStyle:{
+          color:"white"
+        }
+      }} />
       <HomeStack.Screen name ="MediaDetails" component={MediaDetailsScreen}/>
     </HomeStack.Navigator>
   )
@@ -25,9 +32,29 @@ const HomeScreenStack = ()=> {
 export default function App() {
   return (
      <NavigationContainer>
-      <Tab.Navigator initialRouteName='HomeStack'>
-          <Tab.Screen name='HomeStack' component={HomeScreenStack}/>
-          <Tab.Screen name='Profile' component={ProfilePage}/>
+      <Tab.Navigator initialRouteName='HomeStack' screenOptions={{
+        
+        tabBarStyle:{
+          backgroundColor:"#2F2F2F",
+        
+        },
+        tabBarActiveTintColor:"#EE731B"
+      }} >
+          <Tab.Screen name='HomeStack' component={HomeScreenStack} options={{
+            headerShown:false,
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+  
+
+             }}/>
+          <Tab.Screen name='Profile' component={ProfilePage} options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="account" color={color} size={size} />
+          
+            ),
+          
+          }}/>
         </Tab.Navigator>
       </NavigationContainer>
     
